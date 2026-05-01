434fd55e0c11e0c225977bb5632e831ffd305f9d
Subransu <maharanasubhransu6@gmail.com>
2026-05-01 09:24:54 +0530

    add websocket overflow code

---
+++ index.js
@@ -0,0 +1,29 @@
+import http from 'http';
+import {Server} from 'socket.io';
+import express from 'express';
+import path from 'node:path';
+
+async function main(){
+    const app = express();
+    app.use(express.static(path.resolve('./public')));
+    
+    const server = http.createServer(app);
+    const io = new Server();
+
+
+    io.attach(server);
+
+    io.on('connection', (socket) => {
+        console.log('A user connected', socket.id);
+        socket.on('user:message', (message) => {
+            console.log('Received message:', message);
+            socket.broadcast.emit('server:message', message);
+        });
+    })
+    
+    server.listen(9000, () => {
+        console.log(`Server is listening on port 9000`);
+    })
+}
+
+main();
